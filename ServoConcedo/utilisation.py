cfd95f592272573590b00989e9a59fbac0313ad4
Nigel Cleland <nigel.cleland@gmail.com>
2014-03-17 14:27:41 +1300

    Finished transferring most of the functions across

--- ServoConcedo/utilisation.py
+++ ServoConcedo/utilisation.py
@@ -79,3 +79,81 @@ def energy_reserve_matrix(energy, reserve, max_offer):
 
     return combined
 
+
+def asset_utilisation(energy, reserve, filters):
+
+    # Need to remove these or the energy stack won't work
+    reserve_type = filters.pop("Reserve_Type", "FIR")
+    product_type = filters.pop("Product_Type", "PLSR")
+
+    # Get the relevant values
+    capacity = get_cumulative_capacity(energy, filters)
+    estack = get_energy_stack(energy, filters)
+    rstack = get_plsr_stack(reserve, filters, reserve_type=reserve_type, product_type=product_type)
+
+    utilisation = energy_reserve_matrix(estack.values, rstack.values,
+                                        capacity.sum())
+
+    index = ["Energy"] + estack.index.values.tolist()
+    cols = ["Reserve"] + rstack.index.values.tolist()
+
+    # Return it as a DataFrame with prettier indexing and columns
+    util_frame = pd.DataFrame(utilisation, index=index, columns=cols)
+
+    return util_frame, capacity.sum()
+
+
+def relative_utilisation(util, capacity):
+    """
+
+    """
+
+    # Unpack the information
+    index, columns, values = util.index, util.columns, util.copy().values
+
+    # Update the values
+    values[1:, 1:] = values[1:, 1:] / capacity * 100.
+
+    # Repack into a DataFrame
+    return pd.DataFrame(values, index=index, columns=columns)
+
+
+def desired_utilisation(rel, energy, reserve):
+    """
+
+    """
+
+    # Get the appropriate columns and indices
+    columns = [x for x in rel.columns if x <= reserve and x != "Reserve"]
+    index = [x for x in rel.index if x <= energy and x != "Energy"]
+
+    # Wrap in try loop just in case zero offers etc are included
+    try:
+        return np.max(rel[columns].ix[index].values)
+    except:
+        return 0
+
+
+def price_aggregation(prices, agg_func, *args, **kargs):
+    # Get datetime objects if they aren't already
+    if prices["Trading_Date"].dtype != np.datetime64:
+        prices["Trading_Date"] = pd.to_datetime(prices["Trading_Date"])
+
+    # Get the Day of the week
+    prices["Weekdays"] = prices["Trading_Date"].apply(weekday_weekend)
+
+    # Apply the Year Month
+    prices["Year_Month"] = prices["Trading_Date"].apply(year_month)
+
+    # Groupby and return values
+    return prices.groupby(["Year_Month", "Weekdays",
+                            "Trading_Period"])["Price SUM"].aggregate(
+                            agg_func, *args, **kargs)
+
+
+def weekday_weekend(x):
+    return "Weekday" if x.weekday() <= 4 else "Weekend"
+
+
+def year_month(x):
+    return x.strftime('%Y_%m')
